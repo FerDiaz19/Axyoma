9fa9fd35d8e6d3eaa83df340884870addacece45
FerDiaz19 <dfer192003@gmail.com>
2025-08-07 17:22:55 -0700

    cors

--- Backend/config/settings/base.py
+++ Backend/config/settings/base.py
@@ -201,7 +201,7 @@ CORS_ALLOW_CREDENTIALS = True
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:3000",
     "http://127.0.0.1:3000",
-    "http://142.93.183.139"
+    "http://142.93.183.139:3000"
 ]
 
 CORS_ALLOW_HEADERS = [
--- Backend/config/settings/local.py
+++ Backend/config/settings/local.py
@@ -26,7 +26,7 @@ DATABASES = {
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:3000",
     "http://127.0.0.1:3000",
-    "http://142.93.183.139"
+    "http://142.93.183.139:3000"
 ]
 
 CORS_ALLOW_CREDENTIALS = True
