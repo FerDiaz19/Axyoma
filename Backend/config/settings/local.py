81046b766b22f26f5fdb14ee40dbf6782647ea1e
FerDiaz19 <dfer192003@gmail.com>
2025-08-07 17:25:22 -0700

    cors

--- Backend/config/settings/local.py
+++ Backend/config/settings/local.py
@@ -4,7 +4,7 @@ import os
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['localhost', '127.0.0.1', '0.0.0.0', '142.93.183.139']
+ALLOWED_HOSTS = ['*']
 
 import os
 
