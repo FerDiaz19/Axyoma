c396e55acb292fbc097db01b83accbc72129073a
FerDiaz19 <dfer192003@gmail.com>
2025-08-07 17:26:23 -0700

    cors

--- Backend/config/settings/base.py
+++ Backend/config/settings/base.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-tx1+zb7vwker=f)q05g#=9((d!^j#^x%4q_v!cw66_n5(_0-pn
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', '0.0.0.0', '142.93.183.139']
 
 
 # Application definition
