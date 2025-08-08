05e27436fee7f21129edee8164e52dabccb57ef6
FerDiaz19 <dfer192003@gmail.com>
2025-08-07 17:34:04 -0700

    db

--- Backend/config/settings/base.py
+++ Backend/config/settings/base.py
@@ -99,11 +99,11 @@ import os
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': os.getenv('DB_NAME', 'axyomadb'),
-        'USER': os.getenv('DB_USER', 'postgres'),
-        'PASSWORD': os.getenv('DB_PASSWORD', '12345678'),
-        'HOST': os.getenv('DB_HOST', 'localhost'),
-        'PORT': os.getenv('DB_PORT', '5432'),
+        'NAME': os.getenv('DB_NAME'),
+        'USER': os.getenv('DB_USER'),
+        'PASSWORD': os.getenv('DB_PASSWORD'),
+        'HOST': os.getenv('DB_HOST'),
+        'PORT': os.getenv('DB_PORT'),
         'CONN_MAX_AGE': 600,
         'ATOMIC_REQUESTS': True,
     }
